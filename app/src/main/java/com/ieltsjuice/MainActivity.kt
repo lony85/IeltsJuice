c50b0ef97a3d97b5703ee85fae4af1ee728a97d6
lony85 <hamed.dadkhoda@gmail.com>
2022-02-05 15:34:38 +0330

    Prepare For First Release

--- app/src/main/java/com/ieltsjuice/MainActivity.kt
+++ app/src/main/java/com/ieltsjuice/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         replaceFragment(FragmentHome)
 
         binding.bottomNavigation.setOnNavigationItemSelectedListener {
